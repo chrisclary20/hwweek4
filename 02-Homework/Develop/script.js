e5a0d8bc868df5d7c6568d95e7f04cf565648cc9
Chris Clary <chrisclary2016@gmail.com>
2021-07-12 19:19:55 -0400

    Update script.js

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,2 +1,17 @@
-const currentDate = moment().format('MM/DD/YYYY'); // get current date
-document.getElementById(currentDay).textContent = currentDate; // display inside the element
+var currentDate = moment().format('MMMM Do YYYY'); // get current date
+setTimeout(function(){document.getElementById("currentDay").textContent = currentDate;}, 1000) // display inside the element
+
+// color code the time-stamps to indicate past, present, or future
+var h0900 = document.getElementById("9AM");
+var currentTime = moment().format("h A");
+function timeColor() {
+   if (h0900 > currentTime) {
+       document.getElementById("9AM").style.backgroundColor = 'red';
+   } else if (h0900 < currentTime) {
+       document.getElementById("9AM").style.backgroundColor = 'teal';
+   } else if (h0900 == currentTime) {
+       document.getElementById("9AM").style.backgroundColor = 'gold'
+   }
+}
+
+timeColor()
